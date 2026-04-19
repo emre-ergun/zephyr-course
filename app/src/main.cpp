aa2f8b336c24b2f0ea5db0e8576a843f6801e5b2
Emre Ergun <emreergun@engramsoft.com>
2026-04-19 18:35:15 +0300

    feat(app): implement hardware specific configuration via Devicetree and Kconfig. EXTRA_DTC_OVERLAY_FILE should be used

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,7 +3,7 @@
 #include <zephyr/logging/log.h>
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -13,16 +13,20 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&led))
+        return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+        return 0;
 
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+    while (1)
+    {
+        if (gpio_pin_toggle_dt(&led) < 0)
+            return 0;
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
