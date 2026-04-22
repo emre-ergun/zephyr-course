b0a0cf90a41c6d1d318ce03b3b75da61abc58f55
Emre Ergun <emreergun@engramsoft.com>
2026-04-22 21:13:42 +0300

    feat(app): LED application minor changes

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -9,24 +9,22 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-int main(void)
-{
-    bool led_state = true;
+int main(void) {
+  bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led))
-        return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
-        return 0;
-
-    while (1)
-    {
-        if (gpio_pin_toggle_dt(&led) < 0)
-            return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
-    }
+  if (!gpio_is_ready_dt(&led))
     return 0;
+
+  if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+    return 0;
+
+  while (1) {
+    if (gpio_pin_toggle_dt(&led) < 0)
+      return 0;
+
+    led_state = !led_state;
+    LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+    k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+  }
+  return 0;
 }
