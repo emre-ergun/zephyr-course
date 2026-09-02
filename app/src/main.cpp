5c85db642f02a4129bd353dce33b52a31b79aed5
Emre Ergun <emreergun@engramsoft.com>
2026-09-02 23:02:08 +0300

    feat(l2-task1): main application updated to toggle LED every second

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,5 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 
 #define SLEEP_TIME_MS 1000
 
@@ -9,22 +8,18 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
-
-int main(void)
-{
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
-
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
-    }
+int main(void) {
+  if (!gpio_is_ready_dt(&led))
     return 0;
+
+  if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+    return 0;
+
+  while (1) {
+    if (gpio_pin_toggle_dt(&led) < 0)
+      return 0;
+
+    k_msleep(SLEEP_TIME_MS);
+  }
+  return 0;
 }
